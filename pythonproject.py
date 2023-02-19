98d70ce64722f3cbc45f8cad4878949df2ce2f4a
sheridanswanson <125537398+sheridanswanson@users.noreply.github.com>
2023-02-18 16:07:33 -0800

    Update pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,1 +1,15 @@
-#test
+#question 1
+import yfinance as yf
+import pandas as pd
+tesla=yf.Ticker("TSLA")
+import json
+tesla_data=tesla.history(period='max')
+tesla_data.head()
+
+#question 3
+import yfinance as yf
+import pandas as pd
+gme=yf.Ticker("GME")
+import json
+gme_data=gme.history(period='max')
+gme_data.head()
